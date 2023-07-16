c77495b5da040c623edeb12d80cb6459ba31ab54
marouane <marouanmriziq@gmail.com>
2023-07-16 10:13:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry poit
+ *main - entry point
+ *
+ * discription: A C program that prints with put fonction
+ *
  *return: alwyas 0(success)
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
