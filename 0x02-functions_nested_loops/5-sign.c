de56e8da88da43e9fcc807da7aab5e030bb9c3f4
marouane <marouanmriziq@gmail.com>
2023-07-19 11:17:10 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (c > 0)
 	{
 		_putchar(49)
-			return (1);
+		return (1);
 	}
 	else if (c == 0)
 	{
 		_putchar(48)
-			return (0);
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar(45)
-			return (-1)
+		return (-1)
 	}
 }
